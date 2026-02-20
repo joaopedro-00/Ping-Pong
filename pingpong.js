38c184641ca35683433e48da04730ae6e85c564a
joaopedro-00 <pedro.santos.joao2410@escola.pr.gov.br>
2026-02-20 07:47:17 -0300

    Create pingpong.js

---
+++ pingpong.js
@@ -0,0 +1,25 @@
+let raio = 7.5;
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = raio * 2;
+
+let velocidadeXBbolinha = 6;
+let velocidadeYBolinha = 6;
+
+let xRaquete = 5;
+let yRaquete = 150;
+
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+let meusPontos = 0;
+let pontosOponentes = 0;
+
+function setup(){
+ createCanvas(600,400) 
+}
+
+function draw(){
+  
+}
+  
